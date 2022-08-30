5a89cda84acb6c2a9532b7394e05562ad4552104
Alexey Smirnov <Sibusky@yandex.ru>
2022-08-30 13:26:08 +0300

    feat: create routes for login and registration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const { createUser, login } = require('./controllers/users');
 
 const NOT_FOUND_ERROR = 404;
 
@@ -16,16 +17,18 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Middleware для временной авторизации
-app.use((req, res, next) => {
-  req.user = {
-    _id: '62d7bd20c7ccb600853f76d5',
-  };
+// // Middleware для временной авторизации
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: '62d7bd20c7ccb600853f76d5',
+//   };
 
-  next();
-});
+//   next();
+// });
 
 // Использую роуты
+app.post('/signin', login);
+app.post('/signup', createUser);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 app.use('/*', (req, res) => res.status(NOT_FOUND_ERROR).send({ message: 'Страница не найдена' }));
